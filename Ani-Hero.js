1570c57e9a9976d89210714c66ae02ac3f145037
Jonathan Craven <jcraven42@gmail.com>
2018-09-27 16:44:14 -0400

    general cleanup, see notes
    removed console log comment
    fixed tab spacing
    removed back from colors.rand()

--- Ani-Hero.js
+++ Ani-Hero.js
@@ -4,42 +4,41 @@ var divInput;
 var renderedObjects = [];
 var objMax = 1;
 var colors;
+//lines, textS, textX, textY, steps, list, txt, opacity are decalred in Typing.js
 
 function setup() {
-	textX = -1.5*width;//-(width);
-	if(windowWidth<windowHeight){
-	  textS/2
-	  //textX = -2*width;
-	}
+  textX = -1.5*width;
+  if(windowWidth<windowHeight){
+    textS/2
+    //textX = -2*width;
+  }
   colors = {
     back: color(50,25 ,80),
     prime: color(190,85,180),
     dark: color(45,10,50),
     trim: color(45,170,190),
-		text: color(255,255,255),
+    text: color(255,255,255),
     rand: function(){
-      switch(random([/*'back',*/'prime','dark','trim'])){
-        case 'back':
-          return this.back;
-        case 'prime':
-          return this.prime;
-        case 'dark':
-          return this.dark;
-        case 'trim':
-          return this.trim;
-				case 'text':
-          return this.text;
+    switch(random(['prime','dark','trim'])){
+      case 'prime':
+        return this.prime;
+      case 'dark':
+        return this.dark;
+      case 'trim':
+        return this.trim;
+      case 'text':
+        return this.text;
       }
     }
   };
   divHero = select('#ani-hero');
   divInput = select('#input');
-	canvas = createCanvas(windowWidth, windowHeight);
-	divHero.html = "";
+  canvas = createCanvas(windowWidth, windowHeight);
+  divHero.html = "";
   canvas.parent(divHero);
   switch(random(
-    [/*'SpaceFlyer','Rain','SquareWalker',*/'Ripples','Bubbles','OrbitCircle','Collidiscope','ShapeZoom']
-  )){
+      [/*'SpaceFlyer','Rain','SquareWalker',*/'Ripples','Bubbles','OrbitCircle','Collidiscope','ShapeZoom']
+    )){
     case 'SquareWalker':
       objMax = floor(random(3,6));
       for(let i = 0; i<objMax;i++){
@@ -47,10 +46,10 @@ function setup() {
       }
       break;
     case 'OrbitCircle':
-			objMax = 8;
+      objMax = 8;
       for(let i = 1; i<=objMax;i++){
-				renderedObjects.push(new OrbitCircle(QUARTER_PI*i));
-			}
+        renderedObjects.push(new OrbitCircle(QUARTER_PI*i));
+      }
       break;
     case 'Collidiscope':
       objMax = floor(random(3,6));
@@ -59,51 +58,51 @@ function setup() {
       }
       break;
     case 'ShapeZoom':
-      objMax = 3; //floor(random(3,6));
+      objMax = 3;
       for(let i = 0; i<objMax;i++){
         renderedObjects.push(new ShapeZoom());
       }
       break;
     case 'Ripples':
-			console.log('Ripples');
+      console.log('Ripples');
       objMax = floor(random(5,8));
       renderedObjects.push(new Ripple());
-			break;
+      break;
     case 'Bubbles':
-			console.log('Bubbles');
-      objMax = 30; //floor(random(5,8));
+      console.log('Bubbles');
+      objMax = 30;
       renderedObjects.push(new Bubble());
-			break;
+      break;
   }
-	background(colors.back);
+  background(colors.back);
 }
+
 function mouseClicked(){
   if(mouseX > 0 &&
-     mouseX < width &&
-     mouseY > 0 && 
-     mouseY < height){
-       window.location.href = 'https://www.joncraven.com/portfolio';
+    mouseX < width &&
+    mouseY > 0 && 
+    mouseY < height){
+      window.location.href = 'https://www.joncraven.com/portfolio';
   }
 }
 
 function draw() {
   background(colors.back);
-	for(let i = renderedObjects.length-1;i>=0;i--){
+  for(let i = renderedObjects.length-1; i>=0 ;i--){
     renderedObjects[i].show();
-	}
-  for(let i = renderedObjects.length-1;i>=0;i--){
+  }
+  for(let i = renderedObjects.length-1; i>=0 ;i--){
     renderedObjects[i].show();
     let stail = renderedObjects[i].update();
     if (stail===true){
       renderedObjects.splice(i, 1);
     }else if(stail=="new" && renderedObjects.length < objMax){
-			renderedObjects[i].newObj();
-			break;
-		}
+      renderedObjects[i].newObj();
+      break;
+    }
   }
- // console.log(renderedObjects.length);
-	type();
-	if(steps==12){
-		fadeIn();
-	}
+  type();
+  if(steps==12){
+  fadeIn();
+  }
 }
